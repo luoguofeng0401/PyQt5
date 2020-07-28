c52c894b232376283854e11b4149e324ee8cde6b
luoguofeng0401 <56261249+luoguofeng0401@users.noreply.github.com>
2020-07-29 02:27:38 +0800

    20200729
    The first program

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from untitled import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self,setupUi(self)
+
+if __name__=="__main":
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec_())
+
+
+# In[ ]:
+
+
+
+
